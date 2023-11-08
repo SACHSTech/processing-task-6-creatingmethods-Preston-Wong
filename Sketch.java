53cfdae26c1395b58662e2b0ddeefbf6bbac6fd4
Preston-Wong <you@example.com>
2023-11-08 17:25:13 -0500

    Prints out multiple houses with multiple colours

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1300, 1300);
   }
 
   /** 
@@ -24,82 +24,58 @@ public class Sketch extends PApplet {
    */
   public void draw() {
     
-    drawGround(400,400,255);
-    drawHouse(400,400,255);
-    drawRoof(400,400,255);
-    drawWindow(400,400,255);
-    drawDoor(400,400,255);
+    drawHouse(50,100,255,0,0);
+    drawHouse(275,100,255,0,0);
+    drawHouse(500,100,255,165,0);
+    drawHouse(725,100,255,165,0);
+    drawHouse(950,100,255,255,0);
+    drawHouse(50,400,255,255,0);
+    drawHouse(275,400,0,128,0);
+    drawHouse(500,400,0,128,0);
+    drawHouse(725,400,143,0,255);
+    drawHouse(950,400,143,0,255);
+    drawHouse(50,700,0,0,255);
+    drawHouse(275,700,0,0,255);
+    drawHouse(500,700,75,0,130);
+    drawHouse(725,700,75,0,130);
+    drawHouse(950,700,255,0,0);
 
   }
-  
+
   /*
-   * draws the ground of the picture
-   * @param width is how wide the ground will be
-   * @param will determine how high the ground will be
-   * @param colour will decide what colour will be used for which part
+   * draws a house with width and height and colours with as well
+   * @param intHouseWidth is the width of the house, roof, and door
+   * @param intHouseHeight is the height for the house, roof, and door
+   * @param intColour1 is the R value of the RGB code
+   * @param intColour2 is the G value of the RGB code
+   * @param intColour3 is the B value of the RGB code
+   * Draws the house, roof, window, and door with the correct colour 
+   * 
    */
+  
+  public void drawHouse(int intHouseWidth, int intHouseHeight, int intColour1, int intColour2, int intColour3) {
 
-  public void drawGround(int width, int height, int colour) {
-    // Ground
-    fill(colour * 0, (int) (colour * 0.5),colour * 0);
-    rect((float) (width*0),(float) (height/1.025), (float) (width), (float) (height));
-  }
-
-    /*
-   * draws the ground of the picture
-   * @param width is how wide the house will be
-   * @param will determine how high the house will be
-   * @param colour will decide what colour will be used for which part
-   */
-
-  public void drawHouse(int width, int height, int colour) {
-    // Body of the house
-    fill( (int) (colour * 0.94),(int) (colour * 0.98),(int) (colour * 0.74));
-    rect((float) (width/8), (float) (height/2),(float) (width/2), (float) (height/2.1));
-  }
-
-    /*
-   * draws the ground of the picture
-   * @param width is how wide the roof will be
-   * @param will determine how high the roof will be
-   * @param colour will decide what colour will be used for which part
-   */
-
-  public void drawRoof(int width, int height, int colour) {
-    // Roof of the house
-    fill((int) (colour * 0.82),(int) (colour * 0.39),(int) (colour * 0.29));
-    triangle((float) (width/8), (float) (height/2), (float) (width/2.66), (float) (height/4), (float) (width/1.6), (float) (height/2));
-  }
-
-    /*
-   * draws the ground of the picture
-   * @param width is how wide the window will be
-   * @param will determine how high the window will be
-   * @param colour will decide what colour will be used for which part
-   */
-
-  public void drawWindow(int width, int height, int colour) {
-    // Window
-    fill(colour);
-    ellipse((float) (width/2.667),(float) (height/2.58), (float) (width/8), (float) (height/8));
-    line((float) (width/2.285), (float) (height/2.58), (float) (width/3.2),(float) (height/2.58));
-    stroke(colour * 0);
-    line ((float) (width/2.667), (float) (height/2.22), (float) (width/2.667), (float) (height/3.076));
-    stroke(colour * 0);
-  }
-
-    /*
-   * draws the ground of the picture
-   * @param width is how wide the door and handle will be
-   * @param will determine how high the door and handle will be
-   * @param colour will decide what colour will be used for which part
-   */
-
-  public void drawDoor(int width, int height, int colour) {
-    fill((int) (colour * 0.78), (int) (colour * 0.29),colour * 0);
-    rect((float) (width/3.8), (float) (height/1.509), (float) (width/4.44), (float) (height/3.2));
-    fill(colour * 0);
-    ellipse((float) (width/2.19), (float) (height/1.2), (float) (width/40), (float) (height/40));
+	 	  // Body of the house
+       fill(intColour1, intColour2, intColour3);
+       rect(intHouseWidth, intHouseHeight, 200, 190);
+        
+       // Roof of the house
+       fill(intColour1, intColour2, intColour3);
+       triangle(intHouseWidth, intHouseHeight, intHouseWidth + 100, intHouseHeight - 100, intHouseWidth + 200, intHouseHeight);
+   
+       // Window
+       fill(intColour1, intColour2, intColour3);
+       ellipse(intHouseWidth + 100, intHouseHeight - 45,50,50);
+       line(intHouseWidth + 125, intHouseHeight - 45, intHouseWidth + 75,intHouseHeight - 45);
+       stroke(0);
+       line(intHouseWidth + 100, intHouseHeight - 20, intHouseWidth + 100, intHouseHeight - 70);
+       stroke(0);
+   
+       // Door and door handle
+       fill(intColour1, intColour2, intColour3);
+       rect(intHouseWidth + 55, intHouseHeight + 65,90,125);
+       fill(0);
+       ellipse(intHouseWidth + 130, intHouseHeight + 125,10,10);
   }
 
 }
