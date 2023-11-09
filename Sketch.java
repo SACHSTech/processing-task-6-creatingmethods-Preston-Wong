12451cd64bfb7bd73482f6faf2a6449a50442e35
Preston-Wong <you@example.com>
2023-11-09 09:35:42 -0500

    added for loops and colorus for each house

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(135, 206, 235);
+    background(0,0,0);
   }
 
   /**
@@ -24,28 +24,18 @@ public class Sketch extends PApplet {
    */
   public void draw() {
     
-    drawHouse(50,100,255,0,0);
-    drawHouse(275,100,255,0,0);
-    drawHouse(500,100,255,165,0);
-    drawHouse(725,100,255,165,0);
-    drawHouse(950,100,255,255,0);
-    drawHouse(50,400,255,255,0);
-    drawHouse(275,400,0,128,0);
-    drawHouse(500,400,0,128,0);
-    drawHouse(725,400,143,0,255);
-    drawHouse(950,400,143,0,255);
-    drawHouse(50,700,0,0,255);
-    drawHouse(275,700,0,0,255);
-    drawHouse(500,700,75,0,130);
-    drawHouse(725,700,75,0,130);
-    drawHouse(950,700,	50, 205, 50);
-
+    for (int intY= 100; intY< 750; intY+=300) {
+      for (int intX = 50; intX < 1000; intX += 300){ 
+        drawHouse(intX, intY, (intX / 6) + (intY/ 8), (intX / 2) + (intY/ 4),(intX / 4) + (intY / 2));
+        drawPerson(intX, intY,10, (intX / 6) + (intY/ 8), (intX / 2) + (intY / 4),(intX / 4) + (intY / 2));    
+      } 
+    }
   }
 
   /*
    * draws a house with width and height and colours with as well
-   * @param intHouseWidth is the width of the house, roof, and door
-   * @param intHouseHeight is the height for the house, roof, and door
+   * @param intHouseX is the width of the house, roof, and door
+   * @param intHouseY is the height for the house, roof, and door
    * @param intColour1 is the R value of the RGB code
    * @param intColour2 is the G value of the RGB code
    * @param intColour3 is the B value of the RGB code
@@ -53,29 +43,64 @@ public class Sketch extends PApplet {
    * 
    */
 
-  public void drawHouse(int intHouseWidth, int intHouseHeight, int intColour1, int intColour2, int intColour3) {
+  public void drawHouse(int intHouseX, int intHouseY, int intColour1, int intColour2, int intColour3) {
 
-	 	  // Body of the house
-       fill(intColour1, intColour2, intColour3);
-       rect(intHouseWidth, intHouseHeight, 200, 190);
-        
-       // Roof of the house
-       fill(210,100,75);
-       triangle(intHouseWidth, intHouseHeight, intHouseWidth + 100, intHouseHeight - 100, intHouseWidth + 200, intHouseHeight);
+	 	// Body of the house
+    stroke(0);
+    fill(intColour1, intColour2, intColour3);
+    rect(intHouseX, intHouseY, 200, 190);
+       
+    // Roof of the house
+    stroke(0);
+    fill(210,100,75);
+    triangle(intHouseX, intHouseY, intHouseX + 100, intHouseY - 100, intHouseX + 200, intHouseY);
    
-       // Window
-       fill(255);
-       ellipse(intHouseWidth + 100, intHouseHeight - 45,50,50);
-       line(intHouseWidth + 125, intHouseHeight - 45, intHouseWidth + 75,intHouseHeight - 45);
-       stroke(0);
-       line(intHouseWidth + 100, intHouseHeight - 20, intHouseWidth + 100, intHouseHeight - 70);
-       stroke(0);
+    // Window
+    fill(255);
+    ellipse(intHouseX + 100, intHouseY - 45,50,50);
+    line(intHouseX + 125, intHouseY - 45, intHouseX + 75,intHouseY - 45);
+    stroke(0);
+    line(intHouseX + 100, intHouseY - 20, intHouseX + 100, intHouseY - 70);
+    stroke(0);
    
-       // Door and door handle
-       fill(245, 245, 220);
-       rect(intHouseWidth + 55, intHouseHeight + 65,90,125);
-       fill(0);
-       ellipse(intHouseWidth + 130, intHouseHeight + 125,10,10);
+    // Door and door handle
+    fill(245, 245, 220);
+    rect(intHouseX + 55, intHouseY + 65,90,125);
+    fill(0);
+    ellipse(intHouseX + 130, intHouseY + 125,10,10);
+  }
+
+
+  /*
+   * draws a person next to the house
+   * @param intPresonX determines the x point of the person
+   * @param intPersonY determines the y poitn of the person
+   * @param intHead determines the location of the head and head size
+   * intColour1 determiens the R in the RGB value
+   * intColour2 determines the G in the RGB value
+   * intColour3 determines the B in the RGB value
+   * 
+   */
+  
+  public void drawPerson(int intPersonX, int intPersonY, int intHead, int intColour1, int intColour2, int intColour3) {
+
+    // head of the person
+    noFill();
+    stroke(intColour1,intColour2,intColour3);
+    ellipse(intPersonX + 225, intPersonY + 80, 20,20);
+
+    // draws the person's body, legs, and arms 
+    stroke(intColour1,intColour2,intColour3);
+    line(intPersonX + 225, intPersonY + 140, intPersonX + 225, intPersonY + 90);
+    line(intPersonX + 225, intPersonY + 140, intPersonX + 240, intPersonY + 175);
+    line(intPersonX + 225, intPersonY + 140, intPersonX + 210, intPersonY + 175);
+    line(intPersonX + 225, intPersonY + 120, intPersonX + 240, intPersonY + 110);
+    line(intPersonX + 225, intPersonY + 120, intPersonX + 210, intPersonY + 110);
+
+  }
+
+  public int drawDemensions(int intWidth1, int intWidth2) {
+    return intWidth1 - intWidth2;
   }
 
 }
